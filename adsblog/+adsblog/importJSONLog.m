a43f515aea61db509987c118db08851e746c030f
Adrien Perkins <adrienp@stanford.edu>
2019-05-15 17:18:35 -0700

    rearrange a bit

--- adsblog/+adsblog/importJSONLog.m
+++ adsblog/+adsblog/importJSONLog.m
@@ -2,9 +2,8 @@ function aircraftData = importJSONLog(filename)
 % importJSONLog     import a JSON log of ADS-B data into a list of Aircraft
 % types containing all the log data.
 %
-%   aircraftData = adsblog.parser.importJSONLog(filename) imports the
-%   contents of the JSON log specified by the filename (should be a full
-%   path).
+%   aircraftData = adsblog.importJSONLog(filename) imports the contents of
+%   the JSON log specified by the filename (should be a full path).
 
 
 % open the file and decode the json
@@ -14,4 +13,4 @@ fclose(fid);
 val = jsondecode(char(raw'));
 
 % convert from the json struct to list of Aircraft
-aircraftData = jsonStructToAircraft(val);
+aircraftData = adsblog.parser.jsonStructToAircraft(val);
